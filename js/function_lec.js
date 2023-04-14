5c091af10fb684ca044cd6dc4643f0be87a19291
Alexander Royal <alexander.de.royal@gmail.com>
2023-04-14 15:49:57 -0500

    added function-lec html and js file

---
+++ js/function_lec.js
@@ -0,0 +1,26 @@
+// var result = doSomething("Hello");
+// console.log(result);
+//
+// function increment(x){
+//     return x + 1;
+// }
+//
+// let four = increment(3);
+// console.log(four);
+//
+// let six = increment(increment(increment(3)));
+// console.log(six);
+//
+// function increment(someNumber){
+//     return someNumber + 1;
+//     console.log('you will never see this.');
+//     return someNumber + 2;
+// }
+//
+// let increment = function(x){
+//     return x + 1;
+// }
+//
+// let two = increment(1);
+// console.log(two);
+
